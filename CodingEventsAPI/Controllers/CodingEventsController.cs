9927e6fec5c12ca9f05428d64b73c4ba795aaf45
Vamp <vampiirecodes@gmail.com>
2020-04-01 21:38:20 -0500

    base configuration

---
+++ CodingEventsAPI/Controllers/CodingEventsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsAPI.Controllers {
+  [ApiController]
+  [Route(Entrypoint)]
+  public class CodingEventsController : ControllerBase {
+    public const string Entrypoint = "/api/events";
+
+    [HttpGet]
+    public ActionResult GetCodingEvents() {
+      return Ok();
+    }
+  }
+}
